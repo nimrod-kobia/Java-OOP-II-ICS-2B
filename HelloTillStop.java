64bc9a3213c872e19ca000403ce24517f3f45617
Nimrod Kobia <nimrod.kobia@strathmore.edu>
2025-07-07 11:40:40 +0300

    Create HelloTillStop.java

---
+++ HelloTillStop.java
@@ -0,0 +1,24 @@
+package challenges;
+import java.util.*;
+class HelloTillStop{
+        Scanner sc = new Scanner(System.in);
+
+    //outputs Hello while user wants to
+    
+    void showHello(){
+        String choice; //guard variable
+
+        System.out.println("For Hello type h, anything else to stop");
+        choice = sc.next();
+
+        while (("h".equals(choice))) {//check guard
+            System.out.println("Hello");
+            choice = sc.next();
+            
+        }
+    }
+    public static void main(String[] args) {
+        HelloTillStop work = new HelloTillStop();
+        work.showHello();
+    }
+}
